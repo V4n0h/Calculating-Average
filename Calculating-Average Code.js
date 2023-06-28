39d28a5e8369f24d6c94ff40937d1b03d60e5bc4
William Van Fleet <vanfleetwilliam@gmail.com>
2023-06-27 19:44:37 -0500

    get averages

---
+++ Calculating-Average Code.js
@@ -0,0 +1,4 @@
+//Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+
+const findAverage = (array) => array.length !== 0 ? array.reduce((accumulator, currentValue) => accumulator + currentValue) / array.length : 0;
---
+++ Test.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(findAverage([1,1,1]), 1);
+    assert.strictEqual(findAverage([1,2,3]), 2);
+    assert.strictEqual(findAverage([1,2,3,4]), 2.5);
+  });
+});  
